4fac3a3576f228bc742b8c3cdc9a1a153a1ac9b9
marlonx19 <marlon.englemam@gmail.com>
2021-02-05 02:33:05 -0300

    added home screen

--- src/components/customListItem.js
+++ src/components/customListItem.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 
 
-const CustomListItem = () => {
+const CustomListItem = ({ id, chatName, enterChat }) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <ListItem>
+      <Avatar
+        rounded
+        source={{
+          uri: 'https://placebeard.it/360x360'
+        }}
+      />
+      <ListItem.Content>
+        <ListItem.Title style={{ fontWeight: '800' }} >
+          Youtube chat
+        </ListItem.Title>
+        <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
+          this is a test subtitle in order to check what it will look like in a real app
+        </ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
   )
 }
 
 export default CustomListItem;
 
 const styles = StyleSheet.create({
-  
+
 })
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import CustomListItem from '../components/customListItem';
 
+import { auth } from '../../firebase';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Signal',
+      headerStyle: { backgroundColor: '#fff' },
+      headerTitleStyle: { color: 'black' },
+      headerTintColor: 'black',
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+        </View>
+      )
+    })
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        
+        <CustomListItem />
       </ScrollView>
     </SafeAreaView>
   )
@@ -17,7 +35,5 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 })
